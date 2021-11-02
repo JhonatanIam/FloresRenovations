40563d9612c3f716983047a9159ce95e6455c085
Jhonatan Flores <26953546+JhonatanIam@users.noreply.github.com>
2021-11-01 20:12:28 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -123,9 +123,10 @@ jQuery(function ($) {
 			$('.banner-carousel.banner-carousel-1').slick({
 				slidesToShow: 1,
 				slidesToScroll: 1,
+				autoplaySpeed: 5000,
 				autoplay: true,
 				dots: true,
-				speed: 600,
+				speed: 3000,
 				arrows: true,
 				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
 				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
@@ -141,9 +142,10 @@ jQuery(function ($) {
 				fade: true,
 				slidesToShow: 1,
 				slidesToScroll: 1,
+				autoplaySpeed: 5000,
 				autoplay: true,
 				dots: false,
-				speed: 600,
+				speed: 3000,
 				arrows: true,
 				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
 				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
@@ -158,9 +160,10 @@ jQuery(function ($) {
 				fade: true,
 				slidesToShow: 1,
 				slidesToScroll: 1,
+				autoplaySpeed: 5000,
 				autoplay: true,
 				dots: false,
-				speed: 600,
+				speed: 3000,
 				arrows: true,
 				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
 				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
